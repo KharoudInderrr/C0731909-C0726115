9285fc31788220f59cfe747e86f106e001fdf9c4
visualstudio <visualstudio@122-PC28>
2019-03-13 10:12:37 -0400

    first unit test method

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject1
 {
     [TestClass]
-    public class UnitTest1
+    public class BankTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_Withvalidamount_UpdatesBalance()
         {
+            //Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            B
+
         }
     }
 }
